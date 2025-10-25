760f76ee7aadeb9eee1ed99ee6495bc24284a442
proto-lab32 <protoprofit@gmail.com>
2025-10-24 20:53:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MonteCarloSimulator from "./DDSmonte;
+import MonteCarloSimulator from "./DDSmonte";
 
 export default function App() {
   return <MonteCarloSimulator />;
